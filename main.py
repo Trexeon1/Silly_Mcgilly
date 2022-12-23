9a401a1812750d2730d2692a2c9ffd3f815f9189
Elijah Curnutt <curnuttsmart@gmail.com>
2022-12-22 22:16:08 -0700

    Working

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import json
 
 
 def overlay_transparent(background, overlay, x, y):
-
     background_width = background.shape[1]
     background_height = background.shape[0]
 
@@ -39,6 +38,13 @@ def overlay_transparent(background, overlay, x, y):
             ],
             axis=2,
         )
+
+    overlay_image = overlay[..., :3]
+    mask = overlay[..., 3:] / 255.0
+
+    background[y:y + h, x:x + w] = (1.0 - mask) * background[y:y + h, x:x + w] + mask * overlay_image
+
+    return background
 
 
 def get_high_quality_images(keyword):
@@ -67,15 +73,15 @@ def get_keywords(image_link):
     rjson = json.loads(response.content)
     words = {}
 
-    for dict in rjson["image_results"]:
+    for dictionary in rjson["image_results"]:
 
-        for word in dict["title"].split():
+        for word in dictionary["title"].split():
             if word in words:
                 words[word] += 1
             else:
                 words[word] = 1
 
-        for word in dict["snippet"].split():
+        for word in dictionary["snippet"].split():
             if word in words:
                 words[word] += 1
             else:
@@ -92,11 +98,54 @@ def get_keywords(image_link):
 
 
 def place_image(bg_img, new_image, pos):
-    pass
+    if pos == 1:
+        return overlay_transparent(bg_img, new_image, 0, 0)
+    elif pos == 2:
+        return overlay_transparent(bg_img, new_image, 500, 0)
+    elif pos == 3:
+        return overlay_transparent(bg_img, new_image, 0, 500)
+    else:
+        return overlay_transparent(bg_img, new_image, 500, 500)
+
+
+def store_as_opencv_object(image_link):
+
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0"
+    }
+
+    # Makes a request to the image source and stores the byte string of the image
+    new_response = requests.get(image_link, headers=headers)
+
+    # Stores image as OpenCV object
+    nparr = np.frombuffer(new_response.content, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
+
 
 if __name__ == '__main__':
     bg = np.zeros((1000, 1000, 3), dtype=np.uint8)
+    arrows = cv2.imread('overlay.png', cv2.IMREAD_UNCHANGED)
+    images = []
 
+    image_1 = input("What is the link for the starting image? ")
+    images.append(store_as_opencv_object(image_1))
+    image_2 = get_high_quality_images(get_keywords(image_1))
+    images.append(store_as_opencv_object(image_2))
+    image_3 = get_high_quality_images(get_keywords(image_2))
+    images.append(store_as_opencv_object(image_3))
+    image_4 = get_high_quality_images(get_keywords(image_3))
+    images.append(store_as_opencv_object(image_4))
+
+    x = 1
+    for i in images:
+        bg = place_image(bg, i, x)
+        x += 1
+
+    cv2.imwrite("Result.png", bg)
+    cv2.imshow("result", bg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 
